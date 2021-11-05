ef797243845a067f7e3bfea22413f25ab7a316de
Mikhail Shestak <stampout386@gmail.com>
2021-11-05 22:01:38 +0300

    countdown timer

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const daysEl = document.querySelector('#days')
+const hoursEl = document.querySelector('#hours')
+const minutesEl = document.querySelector('#minutes')
+const secondsEl = document.querySelector('#seconds')
+
+const newYears = '12 Feb 2022';
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date()
+
+    const totalSeconds = (newYearsDate- currentDate)/1000;
+    const days = Math.floor(totalSeconds/3600/24);
+    const hours = Math.floor(totalSeconds/3600) % 24;
+    const minutes = Math.floor(totalSeconds/60)%60;
+    const seconds = Math.floor(totalSeconds%60)
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time){
+    return time < 10 ? (`${time}`) : time;
+}
+
+countdown();
+setInterval(countdown,1000)
